953fac0888f45c50a8f38db50ca720f84d37ef50
Vasken Khadar <vaskhder.cs@gmail.com>
2022-01-30 17:10:45 +1100

    setup the server and some mock data

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,34 @@
-//TODO
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+
+const mocks = {
+  Query: {
+    tracksForHome: () => [...new Array(6)],
+  },
+  Track: () => ({
+    id: () => 'track_01',
+    title: () => 'Astro Kitty, Space Explorer',
+    author: () => {
+      return {
+        name: 'Grumpy Cat',
+        photo:
+          'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
+      };
+    },
+    thumbnail: () =>
+      'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
+    length: () => 1210,
+    modulesCount: () => 6,
+  }),
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  mocks,
+});
+
+const port = 4000;
+
+server.listen(port).then(() => {
+  console.log(`server is running on port ${port}`);
+});
