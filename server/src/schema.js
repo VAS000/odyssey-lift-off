542ec681058663a45a788312daf00ed754fb78b6
Vasken Khadar <vaskhder.cs@gmail.com>
2022-02-01 01:21:48 +1100

    add module query field, resolver and getModule function inside trackAPI datasource

--- server/src/schema.js
+++ server/src/schema.js
@@ -5,7 +5,9 @@ const typeDefs = gql`
     "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
     "Fetch a specific track, provided a track's ID"
-    track(id: ID!): Track!
+    track(id: ID!): Track
+    "Fetch a specific module, provided a module's ID"
+    module(id: ID!): Module
   }
 
   "A track is a group of Modules that teaches about a specific topic"
@@ -45,6 +47,10 @@ const typeDefs = gql`
     title: String!
     "The module's length in minutes"
     length: Int
+    "The module's content"
+    content: String
+    "The module's videoUrl"
+    videoUrl: String
   }
 `;
 
